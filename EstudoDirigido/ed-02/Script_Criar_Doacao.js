fdb4b67c53b2320e7c42c426ebdbb4debd8812ad
BrunoCLINIC <bruno@clinic.med.br>
2023-06-21 17:31:29 -0300

    version: Script de Criação de Banco para Coleta de Doação em MongoDB

--- EstudoDirigido/ed-02/Script_Criar_Doacao.js
+++ EstudoDirigido/ed-02/Script_Criar_Doacao.js
@@ -17,10 +17,10 @@ db.createCollection("NecessidadeDoacao")
 db.createCollection("Coleta")
 
 // Cria e Popula os Documents da Collection Usuarios
-db.Usuarios.insertMany([{Id_Usuario: 1, Endereco: [{Rua: "Alexandre Von Humboldt", Complemento: "Sobrado", Cep: "82110000", Cidade: "Curitiba", Estado: "Parana", Telefone: 41999215821}],Email: "bruno.diniz1@utp.edu.br", Tipo: "", Senha: 4209244573},
-                   {Id_Usuario: 2, Endereco: [{Rua: "Padre Tiago Alberione", Complemento: "Casa", Cep: "82020280", Cidade: "Curitiba", Estado: "Parana", Telefone: 41988496995}],Email: "joao.moraes1@utp.edu.br", Tipo: "", Senha: 2879811341},
-                   {Id_Usuario: 3, Endereco: [{Rua: "João Volpe", Complemento: "Casa", Cep: "82320300", Cidade: "Curitiba", Estado: "Parana", Telefone: 41987002769}],Email: "levi.pinho@utp.edu.br", Tipo: "", Senha: 2428505533},
-                   {Id_Usuario: 4, Endereco: [{Rua: "Senador Alencar Guimarães", Complemento: "Apartamento", Cep: "80010-070", Cidade: "Curitiba", Estado: "Parana", Telefone: 41997560527}],Email: "jose.farias1@utp.edu.br", Tipo: "", Senha: 5024156503}]);
+db.Usuarios.insertMany([{Id_Usuario: 1, Endereco: [{Rua: "Alexandre Von Humboldt", Complemento: "Sobrado", Cep: "82110000", Cidade: "Curitiba", Estado: "Parana", Telefone: 41999215821}],Email: "bruno.diniz1@utp.edu.br", Tipo: "Doador", Senha: 4209244573},
+                   {Id_Usuario: 2, Endereco: [{Rua: "Padre Tiago Alberione", Complemento: "Casa", Cep: "82020280", Cidade: "Curitiba", Estado: "Parana", Telefone: 41988496995}],Email: "joao.moraes1@utp.edu.br", Tipo: "Doador", Senha: 2879811341},
+                   {Id_Usuario: 3, Endereco: [{Rua: "João Volpe", Complemento: "Casa", Cep: "82320300", Cidade: "Curitiba", Estado: "Parana", Telefone: 41987002769}],Email: "levi.pinho@utp.edu.br", Tipo: "Coletor", Senha: 2428505533},
+                   {Id_Usuario: 4, Endereco: [{Rua: "Senador Alencar Guimarães", Complemento: "Apartamento", Cep: "80010-070", Cidade: "Curitiba", Estado: "Parana", Telefone: 41997560527}],Email: "jose.farias1@utp.edu.br", Tipo: "Padrinho", Senha: 5024156503}]);
 
 // Cria e Popula os Documents da Collection TipoDoacao
 db.TipoDoacao.insertMany([{Id_TipoDoacao: 1, Descricao: "Ração"},
@@ -29,8 +29,8 @@ db.TipoDoacao.insertMany([{Id_TipoDoacao: 1, Descricao: "Ração"},
 // Cria e Popula os Documents da Collection Doacao
 db.Doacao.insert([{Id_Doacao: 1, Id_Usuario: 1, Data: "30/07/2023", Id_TipoDoacao: 1, Quantidade: "12kg"},
                   {Id_Doacao: 2, Id_Usuario: 2, Data: "12/08/2023", Id_TipoDoacao: 1, Quantidade: "2l,3kg"},
-                  {Id_Doacao: 3, Id_Usuario: 3, Data: "20/10/2023", Id_TipoDoacao: 2, Quantidade: "4,7kg"},
-                  {Id_Doacao: 4, Id_Usuario: 4, Data: "17/02/2023", Id_TipoDoacao: 2, Quantidade: "3kg"}]);
+                  {Id_Doacao: 3, Id_Usuario: 1, Data: "20/10/2023", Id_TipoDoacao: 2, Quantidade: "4,7kg"},
+                  {Id_Doacao: 4, Id_Usuario: 2, Data: "17/02/2023", Id_TipoDoacao: 2, Quantidade: "3kg"}]);
 
 // Cria e Popula os Documents da Collection NecessidadeDoacao
 db.NecessidadeDoacao.insertMany([{Id_Necessidade: 1, Id_Usuario: 1, Data: "20/11/2023", NomeAnimal: "Maggie", Id_TipoDoacao: 1, Quantidade: "12kg"},
